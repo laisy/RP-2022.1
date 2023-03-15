5a2bb7f083a1cef9fe0b054fc61980d1f85081af
David Brito <david.nrpg@gmail.com>
2023-03-15 13:35:11 -0300

    Semana 9 Q4

--- semana 9/Q4.py
+++ semana 9/Q4.py
@@ -1,18 +1,10 @@
 import cv2
 import numpy as np
 
-img1 = cv2.imread(r'c:\Users\David\Desktop\RP-2022.1\semana 9\Fig0230(a)(dental_xray).png', cv2.IMREAD_GRAYSCALE)
-img2 = cv2.imread(r'c:\Users\David\Desktop\RP-2022.1\semana 9\Fig0230(b)(dental_xray_mask).png', cv2.IMREAD_GRAYSCALE)
+img1 = cv2.imread(r'c:\Users\David\Desktop\RP-2022.1\semana 9\Imagens\Fig0230(a)(dental_xray).png', cv2.IMREAD_GRAYSCALE) / 255
+img2 = cv2.imread(r'c:\Users\David\Desktop\RP-2022.1\semana 9\Imagens\Fig0230(b)(dental_xray_mask)2.png', cv2.IMREAD_GRAYSCALE) / 255
 
-resultado = np.zeros_like(img1)
-cv2.imshow('Product', img1)
-cv2.waitKey(3)
-cv2.imshow('Product', img2)
-
-for i in range(img1.shape[0]):
-    for j in range(img1.shape[1]):
-        resultado[i,j] = np.sqrt((img1[i,j] * img2[i,j]))
-
-cv2.imshow('Product', aux)
+resultado = np.multiply(img1, img2)
+cv2.imshow('Product', resultado)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
